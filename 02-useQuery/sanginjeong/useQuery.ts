f6ceafc520f18e0961f6ae0f2e41b4097315bff5
SanginJeong <sanginjeong07@gmail.com>
2026-05-24 21:02:36 +0900

    feat: 02-useQuery 과제 구현(정상인) - export 및 타입 수정

--- 02-useQuery/sanginjeong/useQuery.ts
+++ 02-useQuery/sanginjeong/useQuery.ts
@@ -1,21 +1,20 @@
 import { useEffect, useState, type DependencyList } from "react";
 
 type ResponseType<T> =
-  | { status: "loading"; data: null }
+  | { status: "loading" }
   | { status: "success"; data: T }
   | { status: "error"; error: unknown };
 
-export const useQuery = <T>(
+const useQuery = <T>(
   fn: () => Promise<T>,
   deps: DependencyList = [],
 ): ResponseType<T> => {
   const [response, setResponse] = useState<ResponseType<T>>({
     status: "loading",
-    data: null,
   });
 
   useEffect(() => {
-    setResponse({ status: "loading", data: null });
+    setResponse({ status: "loading" });
     fn()
       .then((res) => {
         setResponse({ status: "success", data: res });
@@ -28,3 +27,5 @@ export const useQuery = <T>(
 
   return response;
 };
+
+export default useQuery;
