97e9cc86153d4e2caa4c19b04390c3ee25ff25f8
Inyoung Cho <cupshop123@gmail.com>
2026-06-08 14:17:41 +0900

    feat: 04-Tabs 과제 구현(조인영)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css';
+import Tabs from "../04-Tabs/join0life/Tabs";
+import "./App.css";
 
 function App() {
-  return <>{/* 아래 본인의 코드를 붙여넣어 테스트 해보세요 */}</>;
+  return (
+    <>
+      <Tabs />
+    </>
+  );
 }
 
 export default App;
