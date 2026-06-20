f7ee41b051413850872b379a80f13a5693535721
le2yunji <le2yunji@naver.com>
2026-06-20 23:38:32 +0900

    feat: initialRating 추가

--- 06-StarRating/le2yunji/StarRating.tsx
+++ 06-StarRating/le2yunji/StarRating.tsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import Star from "./components/Star";
 import "./style/star.css";
 
-export default function StarRating({ maxStars = 5 }) {
-  const [currentRating, setCurrentRating] = useState<number>(0);
-  const [hoveredRating, setHoveredRating] = useState<number>(0);
+export default function StarRating({ maxStars = 5, initialRating = 0 }) {
+  const [currentRating, setCurrentRating] = useState<number>(initialRating);
+  const [hoveredRating, setHoveredRating] = useState<number | null>(null);
 
   const displayRating = hoveredRating || currentRating;
 
   return (
-    <div className="stars" onMouseLeave={() => setHoveredRating(0)}>
+    <div className="stars" onMouseLeave={() => setHoveredRating(null)}>
       {Array.from({ length: maxStars }).map((_, index: number) => {
         const ratingValue = index + 1;
 
