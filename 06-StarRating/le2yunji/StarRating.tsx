5aa9a138356a8bb12752fa4e4730ed075f431cc2
le2yunji <le2yunji@naver.com>
2026-06-23 16:41:22 +0900

    Refac: onChange 매개변수 추가, Star 컴포넌트를 StarIcon과 Star로 분리

--- 06-StarRating/le2yunji/StarRating.tsx
+++ 06-StarRating/le2yunji/StarRating.tsx
@@ -1,12 +1,40 @@
+/**
+ * 별점을 선택할 수 있는 재사용 가능한 Star Rating 컴포넌트입니다.
+ *
+ * 사용자는 별을 클릭하여 rating 값을 선택할 수 있고,
+ * hover 중에는 hover된 별까지 임시로 채워져 보입니다.
+ *
+ * @example
+ * ```tsx
+ * const [rating, setRating] = useState(3);
+ *
+ * <StarRating
+ *   maxStars={5}
+ *   filledStars={rating}
+ *   onChange={setRating}
+ * />
+ * ```
+ */
+
 import { useState } from "react";
 import Star from "./components/Star";
 import "./style/star.css";
 
-export default function StarRating({ maxStars = 5, initialRating = 0 }) {
-  const [currentRating, setCurrentRating] = useState<number>(initialRating);
+type StarRatingProps = {
+  maxStars: number;
+  initialRating: number;
+  onChange: (rating: number) => void;
+};
+
+export default function StarRating({
+  maxStars = 5,
+  initialRating = 0,
+  onChange,
+}: StarRatingProps) {
+  // const [currentRating, setCurrentRating] = useState<number>(initialRating);
   const [hoveredRating, setHoveredRating] = useState<number | null>(null);
 
-  const displayRating = hoveredRating ?? currentRating;
+  const displayRating = hoveredRating ?? initialRating;
 
   return (
     <div className="stars" onMouseLeave={() => setHoveredRating(null)}>
@@ -16,9 +44,10 @@ export default function StarRating({ maxStars = 5, initialRating = 0 }) {
         return (
           <Star
             key={index}
+            ratingValue={ratingValue}
             filled={ratingValue <= displayRating}
-            onMouseEnter={() => setHoveredRating(ratingValue)}
-            onClick={() => setCurrentRating(ratingValue)}
+            onHover={setHoveredRating}
+            onClick={onChange}
           />
         );
       })}
