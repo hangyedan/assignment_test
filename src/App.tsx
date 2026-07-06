b6a13d9d2a87e1eff918638a5652593045222736
Inyoung Cho <cupshop123@gmail.com>
2026-07-06 18:02:54 +0900

    fix: App.tsx 기존 상태로 복원

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,7 @@
-import { useState } from 'react';
-import ImageCarousel from '../08-ImageCarousel/join0life/ImageCarousel';
+import "./App.css";
 
-
-const images = [
-  {
-    src: 'https://picsum.photos/id/600/600/400',
-    alt: 'Forest',
-  },
-  {
-    src: 'https://picsum.photos/id/100/600/400',
-    alt: 'Beach',
-  },
-  {
-    src: 'https://picsum.photos/id/200/600/400',
-    alt: 'Yak',
-  },
-  {
-    src: 'https://picsum.photos/id/300/600/400',
-    alt: 'Hay',
-  },
-  {
-    src: 'https://picsum.photos/id/400/600/400',
-    alt: 'Plants',
-  },
-  {
-    src: 'https://picsum.photos/id/500/600/400',
-    alt: 'Building',
-  },
-];
-
-export default function App() {
-  const [message, setMessage] = useState('Image Carousel');
-
-  return (
-    <div>
-      <h1>{message}</h1>
-      <ImageCarousel images={images} />
-    </div>
-  );
+function App() {
+  return <>{/* 아래 본인의 코드를 붙여넣어 테스트 해보세요 */}</>;
 }
+
+export default App;
